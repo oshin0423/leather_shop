1fd613f8c91ba452d1c59ceb40493c4d2a48c48f
oshin0423 <oshinsomo@gmail.com>
2022-12-11 15:12:09 +0900

    add:header.php作成

---
+++ header.php
@@ -0,0 +1,21 @@
+<body>
+  <header class="l-header">
+    <div class="c-shop">
+        <h1 class="c-shop__icon"><img src ="/wp-content/themes/leather_shop/img/header-logo_pc.png" alt="ヘッダーロゴ" width="52" height="52"></h1>
+        <p class="c-shop__text">店名が入ります</p>
+    </div>
+    <nav class="l-header__nav">
+        <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#concept">Concept</a></li>
+            <li><a href="#product">Product</a></li>
+            <li><a href="#picky">弊社のこだわり</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#summary">会社概要</a></li>
+        </ul>
+    </nav>
+    <div class="l-header__burger-btn">
+        <img src="/wp-content/themes/leather_shop/img/bars_sp.png" alt="ハンバーガーメニューの線">
+        <p>MENU</p>
+    </div>
+  </header> 
