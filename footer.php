a76833df6f4addb7f508b7fb88fac237290bed65
oshin0423 <oshinsomo@gmail.com>
2022-12-11 15:15:50 +0900

    add:footer.php追加

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer class="l-footer">
+        <div class="l-footer__main">
+          <div class="c-shop">
+              <div class="c-shop__icon"><img src ="/wp-content/themes/leather_shop/img/header-logo_pc.png" alt="ヘッダーアイコン" width="52" height="52"></div>
+              <p class="c-shop__text">店名が入ります</p>
+          </div>
+          <nav class="l-footer__main_nav">
+            <ul>
+                <li><a href="#top">Top</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+        <p class="l-footer__copy-right">&#9400;テキストテキスト</p>
+    </footer>
+  </main>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script> 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inview/1.0.0/jquery.inview.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.5.1/dist/simpleParallax.min.js"></script>
+  <script src="/wp-content/themes/leather_shop/./js/main.js"></script> 
+</body>
