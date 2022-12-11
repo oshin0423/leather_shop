526581fef079f890ade427f50ba2cf8d33d1ebd2
oshin0423 <oshinsomo@gmail.com>
2022-12-11 16:56:09 +0900

    add:head,php追加

---
+++ head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta property="og:title" content="株式会社ナカタ | 公式ホームページ">
+  <meta property="og:description" content="レザー専門店の株式会社ナカタです。レザー商品のお取り扱いをご検討の法人様は是非お問合せください。">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://">
+  <meta property="og:image" content="ロゴを入れる">
+  <meta property="og:site_name" content="株式会社ナカタ公式ホームページ">
+  <meta property="og:locale" content="ja_JP">
+  <title>株式会社ナカタ | 公式ホームページ</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
+  <link rel="stylesheet" href="https://use.typekit.net/lhd6lfm.css">
+  <link rel="stylesheet" href="/wp-content/themes/leather_shop/css/style.css">
+  <link rel="stylesheet" href="./css/style.css">
+</head>
